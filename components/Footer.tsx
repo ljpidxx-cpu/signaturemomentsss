1b3f6ec10add0cbb424ea2de92cb18975d833a0d
ljpidxx-cpu <ljpidxx@gmail.com>
2026-08-03 20:24:29 -0500

    Create Footer.tsx

---
+++ components/Footer.tsx
@@ -0,0 +1,23 @@
+export default function Footer() {
+  return (
+    <footer
+      style={{
+        marginTop: "100px",
+        padding: "40px",
+        textAlign: "center",
+        background: "#222",
+        color: "white"
+      }}
+    >
+      <h2>SignatureMoments</h2>
+
+      <p style={{ marginTop: "10px" }}>
+        Handmade crafts made with love ❤️
+      </p>
+
+      <p style={{ marginTop: "25px", opacity: ".7" }}>
+        © 2026 SignatureMoments. All Rights Reserved.
+      </p>
+    </footer>
+  );
+}
